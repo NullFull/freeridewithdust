dff1f9d43fc92c1205377a5c2ce9bfb67d2e9cf0
cmygray <cmygray@gmail.com>
2017-12-05 09:41:33 +0900

    open-api-url debug

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import json
 import webapp2
 import datetime
 from urllib import urlopen
+from config import OPEN_AIR_KEY
 
 
+'''
+GCS = /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+PATH = $PATH;$GCS/bin;
+PYTHONPATH = 
+'''
 
 class MainPage(webapp2.RequestHandler):
     def get_time(self):
@@ -12,14 +18,19 @@ class MainPage(webapp2.RequestHandler):
         return str(datetime.datetime.now()).split(' ')[0]
     def get_average(self):
         f = urlopen(
-            'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?itemCode=PM25&dataGubun=HOUR&searchCondition=WEEK&pageNo=1&numOfRows=30&_returnType=json&ServiceKey=hFfytrBnh8rAAckaVVfx4io3JRk4hFurd5sM4SUf5Fhnea2dOVy8rUlJrHBxN%2BuZYe5vWIvd0g9NldVJu8Bd3g%3D%3D')
+            'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?itemCode=PM25&dataGubun=HOUR&searchCondition=WEEK&pageNo=1&numOfRows=30&_returnType=json&ServiceKey={key}'.format(
+                key=OPEN_AIR_KEY
+            ))
         data = json.load(f)
-        values = [int(row['seoul']) for row in data['list'] if '16:00' > row['dataTime'].split()[1] > '00:00']
+        values = [int(row['seoul']) for row in data['list'] if '16:00' > row['dataTime'].split()[1] > '00:00' and row['seoul'] != '']
         average = sum(values) / len(values)
         return average
 
     def get_forecast(self):
-        f = urlopen('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate={0}&InformCode=PM25&_returnType=json&ServiceKey=hFfytrBnh8rAAckaVVfx4io3JRk4hFurd5sM4SUf5Fhnea2dOVy8rUlJrHBxN%2BuZYe5vWIvd0g9NldVJu8Bd3g%3D%3D'.format(self.get_time()))
+        f = urlopen(
+            'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate={today}&_returnType=json&ServiceKey={key}'.format(
+                today=self.get_time(), key=OPEN_AIR_KEY
+            ))
         data = json.load(f)
         s = data['list'][0]['informGrade']
         grade = s.split(',')[0].split(':')[1].strip()
@@ -31,105 +42,48 @@ class MainPage(webapp2.RequestHandler):
         forecast = self.get_forecast()
 
         if average > 50 and forecast == u'나쁨':
-            self.response.out.write('''
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"  lang="ko" xml:lang="ko">
-<head>
-<title>먼지먹고 무임승차</title>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-</head>
-<style>
- html { height: 100%; }
-  body {
-    margin: auto 0;
-    text-align: center;
-    color: #fff;
-    height: 100%;
-  }
-  h1 {
-    margin: 0;
-  }
-  
-  .bg_Grad01 {
-    padding-top: 40px;
-    height: 100%;
-    background: linear-gradient(to top, #15485d, #f29492);
-  }
-</style>
-
-<body>
-  <div class="bg_Grad01">
-  <h1>먼지먹고 무임승차</h1>
-    <div class="ride_icon">
-      <img src="/static/icon_02.png">
-      <p class="ride_nob">
-          Yes
-      </p>
-    </div>
-    <div class="info">
-      <p>
-        정보제공 : 한국환경정보
-      </p>
-      <p>
-        제작 : 널채움(Nullfull)
-      </p>
-    </div>
-  </div>
-</body>
-
-</html>
-
-                    ''')
+            data = {
+                'message': 'Yeeah~!',
+                'icon': 'icon-on',
+            }
         else:
-            self.response.out.write('''
+            data = {
+                'message': 'Nope',
+                'icon': 'icon-off',
+            }
+
+        self.response.out.write('''
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"  lang="ko" xml:lang="ko">
 <head>
 <title>먼지먹고 무임승차</title>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<link rel="icon" href="/static/favicon.ico">
+<link rel="stylesheet" href="/static/style.css">
 </head>
-<style>
- html { height: 100%; }
-  body {
-    margin: auto 0;
-    text-align: center;
-    color: #fff;
-    height: 100%;
-  }
-  h1 {
-    margin: 0;
-  }
-  
-  .bg_Grad01 {
-    padding-top: 40px;
-    height: 100%;
-    background: linear-gradient(to top, #15485d, #f29492);
-  }
-</style>
+
 <body>
   <div class="bg_Grad01">
   <h1>먼지먹고 무임승차</h1>
     <div class="ride_icon">
-      <img src="/static/icon_02.png">
+      <img src="/static/{icon}.png">
       <p class="ride_nob">
-          Nope
+        {message}
       </p>
     </div>
     <div class="info">
       <p>
-        정보제공 : 한국환경정보
+        정보제공 : <a href="https://www.keco.or.kr/kr/main/index.do">한국환경정보</a>
       </p>
       <p>
-        제작 : 널채움(Nullfull)
+        제작 : <a href="https://www.facebook.com/groupnullfull/">널채움(Nullfull)</a>
       </p>
     </div>
   </div>
 </body>
-
 </html>
-
-                    ''')
-
+'''.format(**data))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
